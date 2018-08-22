3386f10894421dd8f8eae933bec8af16831833d8
Jimmy Smith <jaystansmith@gmail.com>
2018-08-21 20:14:21 -0600

    Added deck and deck_test suite

---
+++ test/deck_test.rb
@@ -0,0 +1,22 @@
+require 'minitest/autorun'
+require 'minitest/pride'
+require './lib/deck'
+
+class DeckTest < MiniTest::Test
+  def test_it_exists
+    card_1 = Card.new("3","Hearts")
+    card_2 = Card.new("4", "Clubs")
+    card_3 = Card.new("5", "Diamonds")
+    deck = Deck.new([card_1, card_2, card_3])
+    assert_instance_of Deck, deck
+  end
+
+  def test_it_has_attributes
+    card_1 = Card.new("3","Hearts")
+    card_2 = Card.new("4", "Clubs")
+    card_3 = Card.new("5", "Diamonds")
+    deck = Deck.new([card_1, card_2, card_3])
+    expected = [card_1, card_2, card_3]
+    assert_equal expected, deck.cards 
+  end
+end
