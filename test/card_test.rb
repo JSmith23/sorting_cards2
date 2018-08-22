af7cb34ef5954a5ec06619cfe369b297e4bb8403
Jimmy Smith <jaystansmith@gmail.com>
2018-08-21 18:35:44 -0600

    Created lib and test directories with card and card test suite

---
+++ test/card_test.rb
@@ -0,0 +1,16 @@
+require 'minitest/autorun'
+require 'minitest/pride'
+require './lib/card'
+
+class CardTest < MiniTest::Test
+  def test_it_exists
+    card = Card.new("Ace", "Spades")
+    assert_instance_of Card, card
+  end
+
+  def test_it_has_attributes
+    card = Card.new("Ace", "Spades")
+    assert_equal "Ace", card.value
+    assert_equal "Spades", card.suit
+  end
+end
