d024c20edb391a393cbf6e4eab74ecca72b76bc6
Jimmy Smith <jaystansmith@gmail.com>
2018-08-21 23:10:52 -0600

    Added modified code

--- lib/card.rb
+++ lib/card.rb
@@ -1,8 +1,20 @@
 class Card
   attr_reader :value,
-              :suit
+              :suit,
+              :card_id 
   def initialize(value,suit)
     @value = value
     @suit = suit
+    @card_id = card_value_id + card_suit_id
+  end
+
+  def card_value_id
+    card_id = {'2' => 2, '3' => 7, '4' => 13, '5' => 18, '6' => 23, '7' => 28, '8' => 32, '9' => 37, '10' => 42, 'Jack' => 47, 'Queen' => 52, 'King' => 57, 'Ace' => 63}
+    card_id[@value]
+  end
+
+  def card_suit_id
+    suit_id = {'Clubs' => 1, 'Diamonds' => 2, 'Hearts' => 3, 'Spades' => 4}
+    suit_id[@suit]
   end
 end
--- lib/deck.rb
+++ lib/deck.rb
@@ -5,4 +5,20 @@ class Deck
   def initialize(cards)
     @cards = cards
   end
+
+  def sort
+    n = cards.length
+
+    loop do
+      swapped = false
+      (n - 1).times do |i|
+        if @cards[i].card_id > @cards[i + 1].card_id
+          @cards[i], @cards[i + 1] = @cards[i + 1], @cards[i]
+          swapped = true
+        end
+      end
+      break if not swapped
+    end
+    @cards
+  end
 end
